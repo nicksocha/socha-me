889327d8e03a4419709af06cfe053e242595e430
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2019-12-02 01:29:59 -0600

    v1.0 - Add 5 sections, back 2 top button, and Analytics. Updated Footer
    Added: Intro, Resume, Skills, Education, and Experience sections, back to top button, Google Analytics. Updated Footer.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-socha-me
+### Welcome to Socha.me
+
+Welcome to [Socha.me](https://socha.me/) where you’ll find the latest and greatest about Nick Socha's career. Including my experience, resume, and contact info. [Socha.me](https://socha.me/) was built in vanilla HTML, CSS, and JS.
+
+<img src="https://github.com/nicksocha/socha-me/blob/master/media/nick-socha-500x500.png?raw=true" alt="Pic of Nick Socha">
--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
+// startBack to top button
+const backToTopButton = document.querySelector('#back-to-top-btn');
+
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 700) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+}
+
+backToTopButton.addEventListener('click', smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    );
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t * t + b;
+  t -= 2;
+  return (c / 2) * (t * t * t + 2) + b;
+}
+// endBack to top button
+
 // Automatically update year in footer
 const yyyy = new Date().getFullYear();
 document.getElementById(
